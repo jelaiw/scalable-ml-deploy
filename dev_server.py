22d91276934e01de408d5060af74292f268c9340
Jelai Wang <jelaiw@uab.edu>
2020-11-20 10:49:25 -0600

    Update picrunch() with docstring and example.
    Refactor picrunch() to separate REST API-oriented "facade" and the
    picrunch algorithm implementation itself to improve readability.

--- dev_server.py
+++ dev_server.py
@@ -3,16 +3,35 @@ from flask import request
 app = Flask(__name__)
 
 @app.route('/api/v0/pi')
-#   Implement picrunch algorithm described by Don Cross as a REST API.
-#
-#   Use Machin's Formula
-#   pi = 4*(4*arctan(1/5) - arctan(1/239))
-#   to calculate pi to ndigit places after the decimal.
+# Implement picrunch algorithm described by Don Cross as a REST API.
 def picrunch():
 	# See https://flask.palletsprojects.com/en/1.1.x/quickstart/#the-request-object.
 	# Default to ndigits of 22.
 	# See https://werkzeug.palletsprojects.com/en/1.0.x/datastructures/#werkzeug.datastructures.MultiDict.
 	ndigits = request.args.get('ndigits', default=22, type=int)
+	return picrunch(ndigits)
+
+def picrunch(ndigits):
+	"""Return value of pi to ndigit places after the decimal.
+
+	Implement picrunch algorithm described by Don Cross.
+
+	Use Machin's Formula
+	pi = 4*(4*arctan(1/5) - arctan(1/239))
+	to calculate pi to ndigit places after the decimal.
+
+	Examples:
+	>>> picrunch(8)
+	ArctanDenom(5) took 14 iterations.
+	ArctanDenom(239) took 4 iterations.
+	{'pi': '3.14159265', 'ndigits': 8}
+
+	Args:
+		ndigits: Number of places after the decimal as an int.
+
+	Returns:
+		Dict with value of pi as a string and ndigits as an int.
+	"""
 	xdigits = 10             # Extra digits to reduce trailing error
 
 	# Use Machin's Formula to calculate pi.
